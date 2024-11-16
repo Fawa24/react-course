2524e5c45d59abf8c5e9bbd8fda812681bbbf76f
Fawa24 <ofeglol@gmail.com>
2024-11-16 19:49:33 +0200

    Refreshed knowledges about passing props to JSX functions

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const options = ['Core', 'Fundamentals', 'Crucial'];
 function getRandomInt(max) {
@@ -20,12 +21,27 @@ function Header() {
   ); 
 }
 
+function CoreConcept(props) {
+  return(
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="The core UI building block" image={componentsImg}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
